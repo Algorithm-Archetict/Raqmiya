05b0ba33ec67fdb6a23c6930d8576a01f674a9a6
algorithm_archetict <elmagnitico2@gmail.com>
2025-07-23 16:14:01 +0300

    Added Shared Layer and updated Projects' references

---
+++ Backend/Shared/DTOs/AuthDTOs/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs.AuthDTOs
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        public string EmailOrUsername { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/Shared/DTOs/ProductDTOs/ReviewDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.DTOs.ProductDTOs
+{
+    public class ReviewDTO { public int Id { get; set; } public int Rating { get; set; } public string? Comment { get; set; } public string UserName { get; set; } = string.Empty; public DateTime CreatedAt { get; set; } }
+}
