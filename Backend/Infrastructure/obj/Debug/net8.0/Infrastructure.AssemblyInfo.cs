956cf10687dee3afdccb3aad3e873f9f50f30c0f
devMahmoudEmad <mahmoudemad7521@gmail.com>
2025-07-27 18:54:16 +0300

    EF queries optmized

--- Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7beb50e10e8bfed545ce0352fd65c0937eebc8d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d313419466318126f5e530a2e6999918d5925a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
