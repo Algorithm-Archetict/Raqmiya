927ff481a907815f60921efd2403369e2988057c
mohamedsherif22 <medo1234sherif@gmail.com>
2025-08-05 18:43:23 +0300

    Added the Reviews Functionality

--- Backend/Shared/DTOs/ProductDTOs/FileDTO.cs
+++ Backend/Shared/DTOs/ProductDTOs/FileDTO.cs
@@ -1,5 +1,10 @@
 ﻿namespace Shared.DTOs.ProductDTOs
 {
     // Nested DTOs (can be in their own files if they get complex)
-    public class FileDTO { public int Id { get; set; } public string Name { get; set; } = string.Empty; public string FileUrl { get; set; } = string.Empty; }
+    public class FileDTO {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FileUrl { get; set; } = string.Empty;
+        public long Size { get; set; } // File size in bytes
+    }
 }
