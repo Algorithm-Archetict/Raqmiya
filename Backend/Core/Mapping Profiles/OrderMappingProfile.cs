c59dfafe3f49d4c2045388f7d71fc07efe85cc72
devMahmoudEmad <mahmoudemad7521@gmail.com>
2025-08-08 19:41:38 +0300

    feat: Enhance OrderDTOs and mapping profile with additional fields and improved mappings

--- Backend/Core/Mapping Profiles/OrderMappingProfile.cs
+++ Backend/Core/Mapping Profiles/OrderMappingProfile.cs
@@ -8,13 +8,38 @@ namespace Core.Mapping_Profiles
     {
         public OrderMappingProfile()
         {
-            CreateMap<Order, OrderDTO>();
-            CreateMap<OrderItem, OrderItemDTO>();
+            CreateMap<Order, OrderDTO>()
+                .ForMember(dest => dest.Subtotal, opt => opt.MapFrom(src => src.Subtotal))
+                .ForMember(dest => dest.Discount, opt => opt.MapFrom(src => src.Discount))
+                .ForMember(dest => dest.Total, opt => opt.MapFrom(src => src.Total))
+                .ForMember(dest => dest.Currency, opt => opt.MapFrom(src => src.Currency))
+                .ForMember(dest => dest.PaymentStatus, opt => opt.MapFrom(src => src.PaymentStatus))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+                .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.OrderItems))
+                .ForMember(dest => dest.CustomerInfo, opt => opt.MapFrom(src => src.CustomerInfo));
+
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.ProductName))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.UnitPrice))
+                .ForMember(dest => dest.Currency, opt => opt.MapFrom(src => src.Currency))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity));
+
             CreateMap<OrderCreateDTO, Order>()
-                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.items));
-            CreateMap<OrderItemCreateDTO, OrderItem>();
-            CreateMap<OrderUpdateDTO, Order>();
+                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.Items))
+                .ForMember(dest => dest.CustomerInfo, opt => opt.MapFrom(src => src.CustomerInfo));
+
+            CreateMap<OrderItemCreateDTO, OrderItem>()
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity));
+
+            CreateMap<OrderUpdateDTO, Order>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
+            CreateMap<CustomerInfo, CustomerInfoDTO>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+                .ForMember(dest => dest.ZipCode, opt => opt.MapFrom(src => src.ZipCode));
         }
     }
 }
-
